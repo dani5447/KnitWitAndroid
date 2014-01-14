bbaf58001ca8075ef5874f8ab7f77266ca39b351
dani5447 <drn5447@rit.edu>
2014-01-13 19:38:53 -0500

    added row counting view
    still have formatting issue -buttons are on top of one another

--- src/com/example/knitwit/MainActivity.java
+++ src/com/example/knitwit/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.knitwit;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	//public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,6 +22,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void countRows(View view){
+		Intent intent = new Intent(this, RowCounter.class);
+	    //EditText editText = (EditText) findViewById(R.id.edit_message);
+	    //String message = editText.getText().toString();
+	    //intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
 	}
 
 }
