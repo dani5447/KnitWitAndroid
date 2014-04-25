590781a4a6da85878e2d575255d4d861097c3a7d
dani5447 <drn5447@rit.edu>
2014-04-25 11:01:21 -0400

    Initial Commit

---
+++ src/com/example/knitwit/RowCounter.java
@@ -0,0 +1,81 @@
+package com.example.knitwit;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class RowCounter extends Activity {
+
+	@SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	    // Get the message from the intent
+	    //Intent intent = getIntent();
+
+	    // Create the text view
+	    //TextView textView = new TextView(this);
+	    //textView.setTextSize(40);
+	    //textView.setText("" + 0);
+	    
+	    //set the background of the text view 
+	    //textView.setBackgroundResource(R.drawable.background3);
+	    
+	    //and set your layout like main content
+	    setContentView(R.layout.activity_row_counter);
+    }
+	
+	public void incrementCounter(View view){
+		//Intent intent = new Intent(this, RowCounter.class);
+		//final TextView FinalCount = (TextView) findViewById(R.id.counter_number);
+	    
+	    //FinalCount.setText("3");
+	}
+	
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.row_counter, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
